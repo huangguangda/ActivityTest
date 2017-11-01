1e49f3c65f7fb4e1939ef99f34909567d5ab43a9
huangguangda <2397923107@qq.com>
2017-11-01 21:20:54 +0800

    添加了Toast用法

--- app/src/main/java/cn/edu/gdmec/android/activitytest/FirstActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activitytest/FirstActivity.java
@@ -3,6 +3,9 @@ package cn.edu.gdmec.android.activitytest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends Activity {
 
@@ -13,5 +16,15 @@ public class FirstActivity extends Activity {
         //去掉标题栏
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView ( R.layout.activity_first );
+        //在活动中使用Toast
+        Button button1=( Button ) findViewById ( R.id.button_1 );
+        button1.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText ( FirstActivity.this, "You clicked Button 1",
+                        Toast.LENGTH_SHORT ).show ();
+                //Toast.LENGTH_SHORT显示时长
+            }
+        } );
     }
 }
