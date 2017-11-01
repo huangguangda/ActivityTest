624d45806717b0bdfd6eaa8cfa97018d5d9bed99
huangguangda <2397923107@qq.com>
2017-11-01 22:52:01 +0800

    在活动中使用Menu

--- app/src/main/java/cn/edu/gdmec/android/activitytest/FirstActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activitytest/FirstActivity.java
@@ -3,7 +3,10 @@ package cn.edu.gdmec.android.activitytest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -27,4 +30,23 @@ public class FirstActivity extends Activity {
             }
         } );
     }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu){
+            getMenuInflater ().inflate ( R.menu.main, menu );
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item){
+            switch (item.getItemId ()){
+                case R.id.add_item:
+                    Toast.makeText ( this, "You clicked Add", Toast.LENGTH_SHORT ).show ();
+                    break;
+                case R.id.remove_item:
+                    Toast.makeText ( this, "You chicked Remove", Toast.LENGTH_SHORT ).show ();
+                    break;
+                default:
+            }
+            return true;
+        }
 }
